ad220557ffd38238e553779d5a8456f2db88de7c
TelmoMS <telmo.marques@mobiteach.fr>
2023-03-25 18:14:06 +0100

    profile details

---
+++ src/app/pages/profile-details/profile-details.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { ProfileDetailsComponent} from "./profile-details.component";
+import {RouterModule} from "@angular/router";
+import {IonicModule} from "@ionic/angular";
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    ProfileDetailsComponent
+  ],
+  exports: [
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ProfileDetailsComponent
+      }
+    ]),
+    IonicModule,
+    ReactiveFormsModule
+  ]
+})
+export class ProfileDetailsModule {
+}
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -11,6 +11,7 @@ import { User } from 'src/app/models/user';
 export class MenuComponent implements OnInit {
   auth = inject(AuthFacade);
   connectedUser : Observable<User> | undefined;
+  activePage = "home";
 
   constructor() { }
 
@@ -31,6 +32,10 @@ export class MenuComponent implements OnInit {
       return '';
     }
     return `@${user.username}`;
+  }
+
+  async changePage(newPage: string) {
+    this.activePage = newPage;
   }
 
   async logoutUser() {
